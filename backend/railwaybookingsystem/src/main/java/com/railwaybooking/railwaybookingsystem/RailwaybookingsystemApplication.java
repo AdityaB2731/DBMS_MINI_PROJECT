7d6d246477857712088252550ae6ccf43b45eff6
AdityaB2731 <aaditya31082005@gmail.com>
2025-10-12 16:52:32 +0530

    spring boot setup

---
+++ backend/railwaybookingsystem/src/main/java/com/railwaybooking/railwaybookingsystem/RailwaybookingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.railwaybooking.railwaybookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwaybookingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwaybookingsystemApplication.class, args);
+	}
+
+}
